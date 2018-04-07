c0e2d9d65256f606e075db6114144c5894715a3d
Malu <malu@example.com>
2018-04-07 18:51:24 +0200

    Env setup

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
-prod:
+prod: docker-down prod-up
+
+prod-up:
+	docker-compose -f docker-compose.yml -f docker-compose-production.yml up -d --build
+
+dev: docker-down dev-up
+
+dev-up:
+	docker-compose up
+
+docker-down:
 	docker-compose down
-	docker-compose up -d --build
--- crontab/crontab.js
+++ crontab/crontab.js
@@ -1,12 +1,27 @@
 const schedule = require('node-schedule');
 const request = require('request');
 
-let gogo = schedule.scheduleJob('0 * * * *', function() {
-        request('http://gogo_web_1/webhooks/send-due-item-reminder', {json: true}, (err, res, body) => {
-            if (err) {
-                return console.log(err);
-            }
-            console.log(body);
-        });
+console.log('Env mode:' + process.env.APP_ENV);
+
+let gogo = schedule.scheduleJob(process.env.CRONTAB_GOGO_SEND_DUE_REMINDER, function() {
+        callWebHook('http://gogo_web_1/webhooks/send-due-item-reminder');
     }
 );
+
+let evett = schedule.scheduleJob(process.env.CRONTAB_EVETT_IMPORT_EVENTS, function() {
+        callWebHook('http://evett_web_1/webhooks/import-events');
+    }
+);
+
+function callWebHook(uri) {
+    if ('dev' === process.env.APP_ENV){
+        console.log('No webhooking in dev mode');
+        return;
+    }
+    request(uri, {json: true}, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(body);
+    });
+}
