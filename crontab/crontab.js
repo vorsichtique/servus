c811317f1c925c4752cf6934995bf46f645a9104
Malu <malu@example.com>
2018-04-10 07:56:35 +0200

    Crontab logging

--- crontab/crontab.js
+++ crontab/crontab.js
@@ -3,17 +3,20 @@ const request = require('request');
 
 console.log('Env mode:' + process.env.APP_ENV);
 
-let gogo = schedule.scheduleJob(process.env.CRONTAB_GOGO_SEND_DUE_REMINDER, function() {
+let gogo = schedule.scheduleJob(process.env.CRONTAB_GOGO_SEND_DUE_REMINDER, function(fireDate) {
+        console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
         callWebHook('http://gogo_web_1/webhooks/send-due-item-reminder');
     }
 );
 
-let evett = schedule.scheduleJob(process.env.CRONTAB_EVETT_IMPORT_EVENTS, function() {
+let evett = schedule.scheduleJob(process.env.CRONTAB_EVETT_IMPORT_EVENTS, function(fireDate) {
+        console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
         callWebHook('http://evett_web_1/webhooks/import-events');
     }
 );
 
-let backuper = schedule.scheduleJob(process.env.CRONTAB_SERVUS_BACKUPER, function() {
+let backuper = schedule.scheduleJob(process.env.CRONTAB_SERVUS_BACKUPER, function(fireDate) {
+    console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
     callWebHook('http://servus_backuper_1/webhooks/run-backup');
 });
 
@@ -23,6 +26,7 @@ function callWebHook(uri) {
         return;
     }
     request(uri, {json: true}, (err, res, body) => {
+        console.log('requesting ' + uri);
         if (err) {
             return console.log(err);
         }
